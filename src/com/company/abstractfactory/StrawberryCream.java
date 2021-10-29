aaa404b3d7cfb940815eb0217cf8b32881bddf82
mohamedtamer0 <mt4452242@gmail.com>
2021-10-30 01:29:02 +0200

    update

--- src/com/company/abstractfactory/StrawberryCream.java
+++
@@ -1,8 +0,0 @@
-package com.company.abstractfactory;
-
-public class StrawberryCream extends CakeCream{
-    @Override
-    public void cream() {
-        System.out.println("Strawberry Cream ");
-    }
-}
