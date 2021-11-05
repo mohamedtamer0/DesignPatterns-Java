b0db7432c3a78999a93a6b9c885edbd27e41a228
mohamedtamer0 <mt4452242@gmail.com>
2021-11-05 03:00:33 +0200

    update

---
+++ src/com/company/strategy/GoToGona.java
@@ -0,0 +1,8 @@
+package com.company.strategy;
+
+public class GoToGona implements Strategy{
+    @Override
+    public void transportation() {
+        System.out.println("take plane");
+    }
+}
---
+++ src/com/company/strategy/Strategy.java
@@ -0,0 +1,5 @@
+package com.company.strategy;
+
+public interface Strategy {
+    void transportation();
+}
